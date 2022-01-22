6380d58ca02b2f80c50aee010393971aafeeb6de
Matt Gleich <git@mattglei.ch>
2022-01-22 14:03:47 -0500

    feat(format): cargo fmt
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/conf.rs
+++ src/conf.rs
@@ -1,48 +1,40 @@
-use std::{fs, path::PathBuf};
+use std::fs;
+use std::path::PathBuf;
 
 use anyhow::Result;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Configuration {
-    pub hostname: String,
-    #[serde(default = "defaults::port")]
-    pub port: u32,
-    #[serde(default = "defaults::destination")]
-    pub destination: PathBuf,
-    pub source: String,
-    #[serde(default = "defaults::username")]
-    pub username: String,
-    #[serde(default = "defaults::password")]
-    pub password: String,
+	pub hostname: String,
+	#[serde(default = "defaults::port")]
+	pub port: u32,
+	#[serde(default = "defaults::destination")]
+	pub destination: PathBuf,
+	pub source: String,
+	#[serde(default = "defaults::username")]
+	pub username: String,
+	#[serde(default = "defaults::password")]
+	pub password: String,
 }
 
 mod defaults {
-    use std::path::PathBuf;
+	use std::path::PathBuf;
 
-    pub fn password() -> String {
-        String::from("")
-    }
-
-    pub fn port() -> u32 {
-        22
-    }
-    pub fn username() -> String {
-        String::from("lvuser")
-    }
-    pub fn destination() -> PathBuf {
-        dirs::desktop_dir().unwrap().join("extractor_logs")
-    }
+	pub fn password() -> String { String::from("") }
+	pub fn port() -> u32 { 22 }
+	pub fn username() -> String { String::from("lvuser") }
+	pub fn destination() -> PathBuf { dirs::desktop_dir().unwrap().join("extractor_logs") }
 }
 
 impl Configuration {
-    pub fn read() -> Result<Self> {
-        let loc = dirs::home_dir()
-            .unwrap()
-            .join(".config")
-            .join("extractor")
-            .join("conf.toml");
-        let content = fs::read_to_string(loc)?;
-        Ok(toml::from_str(&content)?)
-    }
+	pub fn read() -> Result<Self> {
+		let loc = dirs::home_dir()
+			.unwrap()
+			.join(".config")
+			.join("extractor")
+			.join("conf.toml");
+		let content = fs::read_to_string(loc)?;
+		Ok(toml::from_str(&content)?)
+	}
 }
--- src/sftp.rs
+++ src/sftp.rs
@@ -1,4 +1,6 @@
-use std::{fs::create_dir_all, io::Write, process::Command};
+use std::fs::create_dir_all;
+use std::io::Write;
+use std::process::Command;
 
 use anyhow::Result;
 use log::info;
@@ -7,30 +9,30 @@ use which::which;
 use crate::conf::Configuration;
 
 pub fn setup(config: &Configuration) -> Result<()> {
-    if !config.destination.exists() {
-        create_dir_all(&config.destination)?;
-        info!("Destination directory didn't exist so it was created");
-    }
-    which("sftp")?;
-    Ok(())
+	if !config.destination.exists() {
+		create_dir_all(&config.destination)?;
+		info!("Destination directory didn't exist so it was created");
+	}
+	which("sftp")?;
+	Ok(())
 }
 
 pub fn sync(config: &Configuration) -> Result<()> {
-    let loc = format!(
-        "{}@{}.local:{}",
-        config.username, config.hostname, config.source
-    );
-    Command::new("sftp")
-        .arg("-r")
-        .arg(&loc)
-        .arg(&config.destination)
-        .status()?;
-    let mut second_command = Command::new("sftp").arg(&loc).spawn()?;
-    second_command
-        .stdin
-        .as_mut()
-        .unwrap()
-        .write_all("rm logfile-*".as_bytes())?;
-    second_command.wait()?;
-    Ok(())
+	let loc = format!(
+		"{}@{}.local:{}",
+		config.username, config.hostname, config.source
+	);
+	Command::new("sftp")
+		.arg("-r")
+		.arg(&loc)
+		.arg(&config.destination)
+		.status()?;
+	let mut second_command = Command::new("sftp").arg(&loc).spawn()?;
+	second_command
+		.stdin
+		.as_mut()
+		.unwrap()
+		.write_all("rm logfile-*".as_bytes())?;
+	second_command.wait()?;
+	Ok(())
 }
