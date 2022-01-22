83dd2d1a159a86e707ac4407b682cde9fa1e0ace
Matt Gleich <git@mattglei.ch>
2022-01-22 13:21:57 -0500

    feat: sync with sftp and log
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/sftp.rs
+++ src/sftp.rs
@@ -1,14 +1,36 @@
-use std::fs::create_dir_all;
+use std::{fs::create_dir_all, io::Write, process::Command};
 
 use anyhow::Result;
+use log::info;
 use which::which;
 
 use crate::conf::Configuration;
 
-pub fn setup(config: Configuration) -> Result<()> {
+pub fn setup(config: &Configuration) -> Result<()> {
     if !config.destination.exists() {
-        create_dir_all(config.destination)?;
+        create_dir_all(&config.destination)?;
+        info!("Destination directory didn't exist so it was created");
     }
     which("sftp")?;
+    Ok(())
+}
+
+pub fn sync(config: &Configuration) -> Result<()> {
+    let loc = format!(
+        "{}@{}.local:{}",
+        config.username, config.hostname, config.source
+    );
+    Command::new("sftp")
+        .arg("-r")
+        .arg(&loc)
+        .arg(&config.destination)
+        .status()?;
+    let mut second_command = Command::new("sftp").arg(&loc).spawn()?;
+    second_command
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all("rm logfile-*".as_bytes())?;
+    second_command.wait()?;
     Ok(())
 }
